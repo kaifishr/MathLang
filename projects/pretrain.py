a130897f0fcf07b3810452555be22026df51f56a
Kai Fischer <33313471+kaifishr@users.noreply.github.com>
2023-07-31 21:04:47 +0200

    Improve docstring.

--- projects/pretrain.py
+++ projects/pretrain.py
@@ -1,14 +1,12 @@
 """Mathematical expressions pre-training.
 
-Uses mathematical expressions for pre-training followed by training on natural
-language:
+Uses mathematical expressions for pre-training for subsequent training on a
+natural language dataset:
 
 1) Pre-training with mathematical expressions.
-2) Load pre-trained model and adpat it for NLP task.
-    - Remove embedding and classification head. Keep backbone.
-    - Equip backbone with new token and position embedding and classification 
-        head.
-3) Traing model on natural language data.
+2) Use backbone from pre-trained model for new NLP model.
+    - Token embedding, positional embedding and classification head are new.
+3) Traing new model on natural language data.
 
 """
 import torch
@@ -19,7 +17,6 @@ from src.dataloader import get_dataloader
 from src.modules.model import Transformer
 from src.trainer.trainer import Trainer
 from src.utils.tools import set_random_seed
-from src.utils.tools import load_checkpoint
 
 
 def run_training_math(config: Config) -> torch.nn.Module:
@@ -55,14 +52,8 @@ def run_training_lang(math_model: torch.nn.Module, config: Config):
 
     dataloader = get_dataloader(config=config)
     
-    # model = build_model(config=config)
     model = Transformer(config=config)
     model.to(config.trainer.device)
-
-    # ckpt_dir = config.dirs.weights
-    # model_name = config.load_model.model_name
-    # load_checkpoint(model=model, ckpt_dir=ckpt_dir, model_name=model_name)
-    # model.to(config.trainer.device)
 
     # Replace model's backbone with pre-trained weights.
     model.transformer_blocks = math_model.transformer_blocks
@@ -74,26 +65,6 @@ def run_training_lang(math_model: torch.nn.Module, config: Config):
     print("Training finished.")
 
 
-def build_model(config: Config) -> torch.nn.Module:
-    """Build model with backbone from pre-trained model."""
-
-    # Get the model.
-    model = Transformer(config=config)
-
-    # Load model weights.
-    load_checkpoint(
-        model=model,
-        ckpt_dir=config.dirs.weights,
-        model_name=config.load_model.model_name,
-    )
-
-    # Replace token and position embedding. TODO
-    # Replace classification head.
-    # Insert into new model. TODO
-    
-    return model
-
-
 def run_experiment():
     # Seed random number generator.
     set_random_seed(seed=0)
@@ -102,7 +73,7 @@ def run_experiment():
     config = init_config(file_path="config.yml")
 
     # Define pre-training dataset.
-    config.trainer.num_update_steps = 1000
+    config.trainer.num_update_steps = 20000
     config.dataset.dataset = "arithmetic"
     config.load_model.model_name = config.dataset.dataset
 
@@ -110,7 +81,7 @@ def run_experiment():
     math_model = run_training_math(config=config)
 
     # Define training dataset.
-    config.trainer.num_update_steps = 1000
+    config.trainer.num_update_steps = 20000
     config.dataset.dataset = "tinystories"
     config.model.output_sequence_length = 1
 
